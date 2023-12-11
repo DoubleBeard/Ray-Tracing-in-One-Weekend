eff211bacc96e4a992ae2ebe416f3d9cede880a9
Nathaniel Malichi <nathanielmalichi@gmail.com>
2023-12-11 21:20:02 +0200

    Basic PPM Image

--- src/RayTracingInOneWeekend.cpp
+++ src/RayTracingInOneWeekend.cpp
@@ -7,6 +7,24 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+
+	ofstream output("image.ppm");
+
+	int imageWidth = 256;
+	int imageHeight = 256;
+
+	output << "P3" << endl << imageWidth << ' ' << imageHeight << endl << 255 << endl;
+
+	for (double j = 0; j < imageHeight; j++) {
+		clog << "\rScanlines remaining: " << (imageHeight - j) << ' ' << flush;
+		for (double i = 0; i < imageWidth; i++) {
+			int r = static_cast<int>((double)(i / (imageWidth - 1)) * 255.999);
+			int g = static_cast<int>((double)(j / (imageHeight - 1)) * 255.999);
+
+			output << r << ' ' << g << ' ' << 0 << endl;
+		}
+	}
+
+	clog << "\rDone.                  " << endl;
+	output.close();
 }
