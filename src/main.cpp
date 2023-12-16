d258863d3e9e6fbc1451df89baf317c1589aa659
Nathaniel Malichi <nathanielmalichi@gmail.com>
2023-12-16 20:05:57 +0200

    Updates to main.cpp + main.h
    Changed to use STB instead of manually writing P3 images,
    Using HittableList as world and populating it with Spheres
    Naming Convesions changed as well,

--- src/main.cpp
+++ src/main.cpp
@@ -2,80 +2,73 @@
 //
 
 #include "main.h"
-#include "vec3.h"
-#include "ray.h"
-#include "color.h"
 
 const double ASPECT_RATIO = 16.0 / 9.0;
 const double SCALE = 400;
 
-using namespace std;
 
+Color rayColor(const Ray& r, const Hittable& world) {
+	RayHit rec{};
 
-bool hitSphere(const point3& center, double radius, const ray& r) {
-	vec3 oc = r.origin() - center;
-	auto a = dot(r.direction(), r.direction());
-	auto b = 2.0 * dot(oc, r.direction());
-	auto c = dot(oc, oc) - radius * radius;
-	auto discriminant = b * b - 4 * a * c;
-	return discriminant >= 0;
-}
-
-
-color rayColor(const ray& r) {
-	if (hitSphere(point3(0, 0, -1), 0.5, r)) {
-		return color(1, 0, 0);
-	}
+	if (world.hit(r, 0, 100, rec)) {
+		return 0.5 * (rec.normal + Vec3::one());
+	} 
 
 	auto unitDirection = unitVector(r.direction());
 	auto a = 0.5 * (unitDirection.y() + 1.0);  // y is between -1 and 1 so this turns it into 0 to 1.
-	return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);  // Blend values from y = 1 (blue) to y = 0 (white)
+	return (1.0 - a) * Color(1.0, 1.0, 1.0) + a * Color(0.5, 0.7, 1.0);  // Blend values from y = 1 (blue) to y = 0 (white)
 }
 
 
 int main()
 {
-	ofstream output{ "image.ppm" };
-
 	// Using uint8_t instead of regular int (which is 4 bytes long) to avoid overflow warning when deducing from double
 	int imageWidth = static_cast<int>(SCALE);
 	int imageHeight = static_cast<int>(SCALE / ASPECT_RATIO);
+	int channels = 3;
 
 	imageWidth = (imageWidth > 0) ? imageWidth: 1;
 	imageHeight = (imageHeight > 0) ? imageHeight : 1;
 
-	// Camera Settings
 
+	// Camera Settings
 	float focalLength = 1.0;
 	double viewportHeight = 2.0;
 	double viewportWidth = viewportHeight * (static_cast<double>(imageWidth) / imageHeight);
-	auto cameraCenter = point3(0, 0, 0);
+	auto cameraCenter = Point(0, 0, 0);
 
 	// Calucalte the vectors across the horizontal and down the vertical viewport edges
-	auto viewportU = vec3(viewportWidth, 0, 0);
-	auto viewportV = vec3(0, -viewportHeight, 0);
+	auto viewportU = Vec3(viewportWidth, 0, 0);
+	auto viewportV = Vec3(0, -viewportHeight, 0);
 
 	// Calucalte the horizontal and verical delta vectors from pixel to pixel
 	auto pixelDeltaU = viewportU / imageWidth;
 	auto pixelDeltaV = viewportV / imageHeight;
 
 	// Calculate the location of the upper left pixel
-	auto viewportUpperLeft = cameraCenter - vec3(0, 0, focalLength) - viewportU / 2 - viewportV / 2;  // Take camera position, add focal length as how deep is the viewport, then move left and up (u is towards right and v is towards down)
+	auto viewportUpperLeft = cameraCenter - Vec3(0, 0, focalLength) - viewportU / 2 - viewportV / 2;  // Take camera position, add focal length as how deep is the viewport, then move left and up (u is towards right and v is towards down)
 	auto pixel00Loc = viewportUpperLeft + 0.5 * (pixelDeltaU + pixelDeltaV);  // Use upper left corner and move half U delta and half V delta
 
-	output << "P3" << endl << imageWidth << ' ' << imageHeight << endl << 255 << endl;
+	// World Settings
+	HittableList world;
+	world.add(make_shared<Sphere>(Point(0, 0, -1), 0.5));
+	world.add(make_shared<Sphere>(Point(0, -100.5, -1), 100));
 
+	
+	vector<unsigned char> image(imageWidth * imageHeight * channels);
 	for (int j = 0; j < imageHeight; j++) {
 		clog << "\rScanlines remaining: " << (imageHeight - j) << ' ' << flush;
 		for (int i = 0; i < imageWidth; i++) {
 			auto pixelCenter = pixel00Loc + (i * pixelDeltaU) + (j * pixelDeltaV);
 			auto rayDirection = pixelCenter - cameraCenter;
-			ray r{ cameraCenter, rayDirection };
+			Ray r{ cameraCenter, rayDirection };
 
-			writeColor(output, rayColor(r));
+			int index = (j * imageWidth + i) * channels;
+			writeColor(image, index, rayColor(r, world));
 		}
 	}
 
 	clog << "\rDone.                  " << endl;
-	output.close();
+
+	stbi_write_png("output.png", imageWidth, imageHeight, channels, image.data(), imageWidth * channels);
 }
--- src/main.h
+++ src/main.h
@@ -3,8 +3,27 @@
 
 #pragma once
 
+
+#include "vec3.h"
+#include "ray.h"
+#include "color.h"
+#include "sphere.h"
+#include "hittable.h"
+#include "hittable_list.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "third-party/stb/stb_image_write.h"
+
+#include <memory>
 #include <iostream>
 #include <fstream>
 #include <filesystem>
 
-// TODO: Reference additional headers your program requires here.
+using namespace std;
+
+const double DOUBLE_INFINITY = numeric_limits<double>::infinity();
+const double PI = 3.1415926535897932385;
+
+inline double degToRads(double degrees) {
+	return degrees * PI / 180.0;
+}
