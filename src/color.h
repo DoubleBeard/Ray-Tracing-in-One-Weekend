5483b33b7c827808d634cb176442210f918218be
Nathaniel Malichi <nathanielmalichi@gmail.com>
2023-12-22 11:25:24 +0200

    Conversion from Linear space to Gamma space

--- src/color.h
+++ src/color.h
@@ -10,12 +10,16 @@ using namespace std;
 
 using Color = Vec3;
 
+inline double linearToGamma(double linearComponent) {
+    return sqrt(linearComponent);
+}
+
 void writeColor(vector<unsigned char>& image, int index, Color pixelColor, int samplesPerPixel) {
     static const Interval intensity(0, 0.999);  // Just for making sure result is withing 0-1
 
-    auto r = pixelColor.x() / samplesPerPixel;
-    auto g = pixelColor.y() / samplesPerPixel;
-    auto b = pixelColor.z() / samplesPerPixel;
+    auto r = linearToGamma(pixelColor.x() / samplesPerPixel);
+    auto g = linearToGamma(pixelColor.y() / samplesPerPixel);
+    auto b = linearToGamma(pixelColor.z() / samplesPerPixel);
 
     image[index + 0] = static_cast<unsigned char>(256 * intensity.clamp(r));
     image[index + 1] = static_cast<unsigned char>(256 * intensity.clamp(g));
